02abc68a6e0003dc0ef6d38fb3b0d14e378da64c
Chino Franco <chino.franco@gmail.com>
2025-05-09 11:34:58 +0900

    fix: write status code earlier

--- service/handlers.go
+++ service/handlers.go
@@ -8,12 +8,12 @@ import (
 // CreateNewMockHandler instantiates a new HTTP handler for a mock endpoint.
 func CreateNewMockHandler(statusCode int, content any) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(statusCode)
 		err := json.NewEncoder(w).Encode(content)
 		if err != nil {
 			http.Error(w, "Failed to serve mock endpoint", http.StatusInternalServerError)
 			return
 		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(statusCode)
 	}
 }
