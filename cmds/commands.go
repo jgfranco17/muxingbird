28907ab9efa989963db3f7c87939c92bb12702ff
Chino Franco <chino.franco@gmail.com>
2025-05-11 19:09:02 +0900

    feat: implement init command

--- cmds/commands.go
+++ cmds/commands.go
@@ -2,12 +2,14 @@ package cmds
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/jgfranco17/muxingbird/errorx"
 	"github.com/jgfranco17/muxingbird/logging"
+	"github.com/jgfranco17/muxingbird/service"
 	"github.com/spf13/cobra"
 )
 
@@ -50,5 +52,50 @@ func CommandRun(serviceFactory ServiceFactory) *cobra.Command {
 	}
 	cmd.Flags().DurationVarP(&activeDuration, "duration", "d", defaultMaxDuration, "Maximum duration to run server")
 	cmd.Flags().IntVarP(&port, "port", "p", defaultPort, "Port to run server on")
+	return cmd
+}
+
+// CommandInit creates a new Cobra command for initializing
+// a starter config with basic routes.
+func CommandInit() *cobra.Command {
+	var outputPath string
+	var force bool
+	cmd := &cobra.Command{
+		Use:   "init",
+		Short: "Generate a starter mock config file",
+		Long:  "Create a basic starter server definition file for the user to fill.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			logger := logging.FromContext(cmd.Context())
+			ctx, cancel := context.WithCancel(cmd.Context())
+			defer cancel()
+			if outputPath == "" {
+				if err := service.InitConfig(ctx, cmd.OutOrStdout()); err != nil {
+					return errorx.NewErrorWithCode(err, errorx.ExitGenericError)
+				}
+				return nil
+			}
+			if _, err := os.Stat(outputPath); err == nil && !force {
+				coreErr := fmt.Errorf("file %q already exists (use --force to overwrite)", outputPath)
+				return errorx.NewErrorWithCode(coreErr, errorx.ExitInvalidArgs)
+
+			}
+			file, err := os.Create(outputPath)
+			if err != nil {
+				return fmt.Errorf("failed to create file: %w", err)
+			}
+			defer func() {
+				if cerr := file.Close(); cerr != nil {
+					logger.Fatalf("Failed to close file: %v", cerr)
+				}
+			}()
+			if err := service.InitConfig(ctx, file); err != nil {
+				return errorx.NewErrorWithCode(err, errorx.ExitGenericError)
+			}
+			logger.Infof("Created new default config: %s", outputPath)
+			return nil
+		},
+	}
+	cmd.Flags().StringVarP(&outputPath, "output", "o", "", "Path to write config file (default: stdout)")
+	cmd.Flags().BoolVarP(&force, "force", "f", false, "Overwrite existing file if it exists")
 	return cmd
 }
